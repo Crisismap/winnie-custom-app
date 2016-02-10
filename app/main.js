76d5a37597367097462b932733cd167882024ae9
zverev <alexander.zverev@gmail.com>
2016-02-10 13:41:46 +0300

    initial

---
+++ app/main.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+    $.ajax('user/config.json').then(function (config) {
+        var container = $('body')[0];
+        var winnieCm = nsGmx.createGmxApplication(container, config);
+        winnieCm.create().then(function () {
+            console.log('ready');
+            window.runUserScript.call(this, winnieCm);
+        }, function (err) {
+            console.log('failed to create gmxApplication');
+        })
+    }, function () {
+        console.error('config not found');
+    });
+});
---
+++ user/script.js
@@ -0,0 +1,9 @@
+function runUserScript(cm) {
+    // TODO: add script here
+    // you can get components via component manager
+    // like this:
+
+    var map = cm.get('map');
+    var layersHash = cm.get('layersHash');
+    var layersTree = cm.get('layersTree');
+}
